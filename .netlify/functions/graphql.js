8dbffdde5e5156403456b029e94c1da94cf6dfad
bozidar <bozidar.siljanoski@live.com>
2020-08-24 19:32:17 +0200

    meh

---
+++ .netlify/functions/graphql.js
@@ -0,0 +1,24 @@
+const { ApolloServer , gql } = require("apollo-server-lambda");
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (obj, args, context) => {
+      return "Hello, world!";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+});
+
+exports.handler = server.createHandler();
